bfcfca37c0b1756d2cf2e07658b5e03bd82def47
Daniil razornd Razorenov <daniltmb@gmail.com>
2019-08-22 22:51:59 +0300

    create speedometer component

--- src/frontend/src/app/components/index.ts
+++ src/frontend/src/app/components/index.ts
@@ -24,22 +24,25 @@ import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 import {CommentsListComponent} from './comment/comments-list.component';
 import {PipesModule} from '../pipes/pipes.module';
 import {PostsListComponent} from './posts-list/posts-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from '@angular/forms';
 import {PostsFilterComponent} from './posts-filter/posts-filter.component';
-import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 import {DashboardCardComponent} from './dashboard-card/dashboard-card.component';
-import {MatMenuModule} from '@angular/material/menu';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
+import {SpeedometerComponent} from './speedometer/speedometer.component';
+import {NgxGaugeModule} from 'ngx-gauge';
 
 export const COMPONENTS = [
   DashboardCardComponent,
   PostComponent,
   PostsListComponent,
   PostsFilterComponent,
-  CommentsListComponent
+  CommentsListComponent,
+  SpeedometerComponent
 ];
 
 @NgModule({
@@ -47,6 +50,7 @@ export const COMPONENTS = [
     BrowserAnimationsModule,
     CommonModule,
     ReactiveFormsModule,
+    NgxGaugeModule,
     MatCardModule,
     MatListModule,
     MatInputModule,
